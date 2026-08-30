cf16499390709aca46c2ba81f6b585d4e54b6e48
jeyanthm06-create <jeyanthm06@gmail.com>
2026-08-30 22:41:40 +0530

    init: folder structure and solution template

---
+++ template.c
@@ -0,0 +1,10 @@
+/*
+ * Problem: 
+ * Link: https://leetcode.com/problems/
+ * Topic: 
+ * Pattern: 
+ * Difficulty: Easy / Medium / Hard
+ * Time Complexity: O()
+ * Space Complexity: O()
+ * Date: 
+ */
